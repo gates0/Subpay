6acf6c5a260ee5d75cace457e16f76b123d8ef14
Oluwatimilehin Festus-Osho <guyosho@gmail.com>
2026-03-29 14:52:49 +0100

    Database URL

--- db/session.py
+++ db/session.py
@@ -5,13 +5,13 @@ from config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-# if DATABASE_URL.startswith("postgres://"):
-#     DATABASE_URL = DATABASE_URL.replace(
-#         "postgres://",
-#         "postgresql+psycopg2://",
-#         1
-#     )
-engine = create_engine(DATABASE_URL)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+psycopg2://",
+        1
+    )
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
