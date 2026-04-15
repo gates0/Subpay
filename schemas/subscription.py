d59f11ed97d551558d58e054b9434fa73af9331a
Oluwatimilehin Festus-Osho <guyosho@gmail.com>
2026-04-15 23:41:33 +0100

    Subscribers fix

--- schemas/subscription.py
+++ schemas/subscription.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from enum import Enum
 from typing import Optional
+from uuid import UUID
 
 from pydantic import BaseModel
 
@@ -36,7 +37,7 @@ class HubSummary(BaseModel):
 
 
 class MemberSummary(BaseModel):
-    id: int
+    id: UUID
     username: str
     avatar_url: Optional[str] = None
 
